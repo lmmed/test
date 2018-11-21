52956927f01e8b685c1863c7e61882a1f924cfe8
lmmed <mohamed.lamhaouar@gmail.com>
2018-11-21 18:22:27 +0100

    test

---
+++ src/main/java/test/main/Main.java
@@ -0,0 +1,17 @@
+package test.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = true;
+		if (a) {
+			System.out.println("zzeze");
+		}
+		if (a) {
+			System.out.println("hhh");
+		}
+
+	}
+
+}
---
+++ src/main/java/test/main/NovelisClass.java
@@ -0,0 +1,21 @@
+package test.main;
+
+public class NovelisClass {
+
+	public String novelis(Integer test) {
+		System.out.println("aaa");
+		if(test!=null) {
+			return "test";
+		}
+		if (test == 1) {
+			return "test";
+		} else if (test == 1) {
+			return "aa";
+		} else if (test == 2) {
+			return "a";
+		}
+		return "bb";
+
+	}
+
+}
